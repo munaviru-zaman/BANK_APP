e46071077cf0c8d84c3fe62ecb240fbce5e128a4
zamancode <triumphvaydor7@gmail.com>
2022-09-16 17:45:43 +0530

    16-09-2022 funtions add login-page

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+accountNumber:number=0
+password:any=''
+database:any={
+  1000:{
+    acno:1000,password:1000
+  }
+}
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  accountno(event:any){
+    this.accountNumber=event.target.value
+    console.log(this.accountNumber)
+  }
+
+  passWord(event:any){
+    this.password=event.target.value
+    console.log(this.password);
+    
+  }
+
+  login(){
+    var acno=this.accountNumber
+    var pswd=this.password
+    if(acno in this.database){
+      if(pswd==this.database[acno]["password"]){
+        alert("LOGIN SUCCESSFULLY")
+      }else{
+        alert("INCORRECT PASSWORD")
+      }
+    }else{
+      alert("USER DOES NOT EXISTS")
+    }
   }
 
 }
